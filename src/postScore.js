13ca83e687a0f52ee72d3051601ec71701840036
JuliCarracedo <julicarra@hotmail.com>
2021-09-02 15:48:11 -0300

    Run linters

--- src/postScore.js
+++ src/postScore.js
@@ -2,9 +2,9 @@ const postScore = (score, i) => {
   const table = document.querySelector('div.leaderboard');
   const newDiv = document.createElement('div');
   newDiv.classList.add('py-2', 'px-2', 'score');
-  if(i === 0){newDiv.classList.add('first');}
-  if(i === 1){newDiv.classList.add('second');}
-  if(i === 2){newDiv.classList.add('third');}
+  if (i === 0) { newDiv.classList.add('first'); }
+  if (i === 1) { newDiv.classList.add('second'); }
+  if (i === 2) { newDiv.classList.add('third'); }
   newDiv.innerHTML = `
         <p><strong>${score.user}:</strong> ${score.score}</p>
     `;

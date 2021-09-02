2220024c65da5535db1bdc764e0d4b481a7ead35
JuliCarracedo <julicarra@hotmail.com>
2021-09-02 15:42:59 -0300

    add styling

--- src/loadStored.js
+++ src/loadStored.js
@@ -4,7 +4,7 @@ const loadStored = (list) => {
   list = list.sort((obj1, obj2) => parseInt(obj2.score, 10) - parseInt(obj1.score, 10));
   if (list.length === 0) { return; }
   for (let i = 0; i < list.length; i += 1) {
-    postScore(list[i]);
+    postScore(list[i], i);
   }
 };
 
--- src/postScore.js
+++ src/postScore.js
@@ -1,10 +1,12 @@
-const postScore = (score) => {
+const postScore = (score, i) => {
   const table = document.querySelector('div.leaderboard');
   const newDiv = document.createElement('div');
-  newDiv.classList.add('my-1', 'mx-2');
+  newDiv.classList.add('py-2', 'px-2', 'score');
+  if(i === 0){newDiv.classList.add('first');}
+  if(i === 1){newDiv.classList.add('second');}
+  if(i === 2){newDiv.classList.add('third');}
   newDiv.innerHTML = `
         <p><strong>${score.user}:</strong> ${score.score}</p>
-        <hr>
     `;
   table.appendChild(newDiv);
 };
